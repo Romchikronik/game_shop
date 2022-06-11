f872026a8d5cbdcabcc0de8653462571d8d8e319
Romchik <romankolesov44@gmail.com>
2022-06-11 14:20:42 +0500

    Update project

--- megashop/settings.py
+++ megashop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-de$rrjd%)6k^wi)se-pj!t*+sf!3bn8hcq%06%dp5#@77!v6-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False  # изменить на True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'github.com/Romchikronik/game_shop']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
